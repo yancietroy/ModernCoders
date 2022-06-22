5d6837cf94d8e146b950af43577ed249057ea78d
bienny <106976531+bienny@users.noreply.github.com>
2022-06-22 22:14:01 +0800

    Update register.php

--- register.php
+++ register.php
@@ -6,11 +6,10 @@
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>JRU Student Organizations Portal Login Page</title>
   <link rel="stylesheet" type="text/css" title="stylesheet" href="assets/css/style.css">
-
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
-
 <body class="bg">
+  <form action="registered.php" method="post">
   <section class="h-100">
     <div class="container py-5 h-100">
       <div class="row justify-content-center align-items-center h-100">
@@ -26,7 +25,7 @@
                   <div class="col-md-4 mb-4">
                     <div class="form-outline">
                       <label class="form-label" for="firstName" id="asterisk">First name</label>
-                      <input type="text" id="FirstName" class="form-control form-control-lg" required />
+                      <input type="text" name="first_name" id="FirstName" class="form-control form-control-lg" required />
                       <div class="valid-feedback"></div>
                       <div class="invalid-feedback">First name field cannot be blank!</div>
                       <div class="help-block with-errors"></div>
@@ -36,7 +35,7 @@
                     <div class="form-outline">
 
                       <label class="form-label" for="middleName">Middle name</label>
-                      <input type="text" id="middleName" class="form-control form-control-lg" />
+                      <input type="text" name="middle_name" id="middleName" class="form-control form-control-lg" />
                       <div class="valid-feedback">
                         Looks good!
                       </div>
@@ -47,7 +46,7 @@
                     <div class="form-outline">
 
                       <label class="form-label" for="lastName" id="asterisk">Last name</label>
-                      <input type="text" id="lastName" class="form-control form-control-lg" required />
+                      <input type="text" name="last_name" id="lastName" class="form-control form-control-lg" required />
                       <div class="valid-feedback">Looks Good!</div>
                       <div class="invalid-feedback">Last name field cannot be blank!</div>
                     </div>
@@ -57,7 +56,7 @@
                   <div class="col-md-6 mb-4">
                     <div class="form-outline">
                       <label class="form-label" for="age" id="asterisk">Age</label>
-                      <input type="text" id="age" class="form-control form-control-lg" required />
+                      <input type="text" name="age" id="age" class="form-control form-control-lg" required />
                       <div class="valid-feedback">Looks Good!</div>
                       <div class="invalid-feedback">Age field cannot be blank!</div>
                     </div>
@@ -65,12 +64,12 @@
                   <div class="col-md-6 mb-4">
                     <label class="mb-4 me-5 w-50" for="gender" id="asterisk">Gender: </label>
                     <div class="form-check form-check-inline mb-0 me-4 ">
-                      <input class="form-check-input" type="radio" name="gender" id="Male" autocomplete="off" />
-                      <label class="form-check-label" for="femaleGender">Male</label>
+                      <input class="form-check-input" type="radio" name="gender" id="Male" value="Male" autocomplete="off" />
+                      <label class="form-check-label" for="maleGender">Male</label>
                     </div>
 
                     <div class="form-check form-check-inline mb-0 me-4">
-                      <input class="form-check-input" type="radio" name="gender" id="Female" autocomplete="off" />
+                      <input class="form-check-input" type="radio" name="gender" id="Female" value="Female" autocomplete="off" />
                       <label class="form-check-label" for="Female">Female</label>
                     </div>
                     <div class="valid-feedback mv-up">You selected a gender!</div>
@@ -82,7 +81,7 @@
                   <div class="col-4 mb-4">
 
                     <label class="form-label select-label" id="asterisk">College</label>
-                    <select class="form-select form-select-sm" required>
+                    <select class="form-select form-select-sm" name="college" id="college" required>
                       <option selected disabled value="" text-muted>Select College</option>
                       <option value="1">One</option>
                       <option value="2">Two</option>
@@ -96,7 +95,7 @@
                   <div class="col-4 mb-4">
 
                     <label class="form-label select-label" id="asterisk">Course</label>
-                    <select class="form-select form-select-sm" s required>
+                    <select class="form-select form-select-sm" name="course" id="courses" s required>
                       <option selected disabled value="" text-muted>Select Course</option>
                       <option value="1">One</option>
                       <option value="2">Two</option>
@@ -109,7 +108,7 @@
                   <div class="col-4 mb-4">
 
                     <label class="form-label select-label" id="asterisk">Organization</label>
-                    <select class="form-select form-select-sm" required>
+                    <select class="form-select form-select-sm" name="org" id="org" required>
                       <option selected disabled value="" text-muted>Select Organization</option>
                       <option value="1">One</option>
                       <option value="2">Two</option>
@@ -135,7 +134,7 @@
                     <div class="form-outline">
 
                       <label class="form-label" for="password" id="asterisk">Password</label>
-                      <input type="text" class="form-control" id="password" data-bs-toggle="popover" data-bs-placement="bottom" title="Password Validation" minlength="8" data-bs-content="Must be at least 8 characters long &#013;
+                      <input type="password" class="form-control" name="password" id="password" data-bs-toggle="popover" data-bs-placement="bottom" title="Password Validation" minlength="8" data-bs-content="Must be at least 8 characters long &#013;
                 Must contain at least one number &#013;
                       Must contain at least one special character &#013;
                       and must contain at least one uppercase and lowercase letter" required>
@@ -149,19 +148,19 @@
                     <div class="form-outline">
 
                       <label class="form-label" for="Confirmpassword" id="asterisk">Confirm Password</label>
-                      <input type="text" class="form-control" id="Confirmpassword" minlength="3" required>
+                      <input type="password" class="form-control" name="confirmpassword" id="Confirmpassword" minlength="3" required>
                     </div>
                   </div>
                 </div>
 
                 <div class="col-md-12 mb-4">
-                  <button class="w-100 btn btn-lg btn-primary mt-4" type="submit" value="register">Register</button>
+                  <button class="w-100 btn btn-lg btn-primary mt-4" type="submit" name="submit" value="register">Register</button>
 
                 </div>
 
               </form>
               <hr class="my-4">
-              <p class="mt-3 text-center">Already have an account? <a href="login.html" class="text-blue-50 fw-bold">Login</a>
+              <p class="mt-3 text-center">Already have an account? <a href="login.php" class="text-blue-50 fw-bold">Login</a>
               </p>
 
             </div>
@@ -179,6 +178,7 @@
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
     const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
   </script>
+  </form>
 </body>
 
 </html>
