40894d32eb4c694ee15589ce7ee598be78de1162
stycroft <98692180+stycroft@users.noreply.github.com>
2022-06-25 14:34:50 +0800

    Revert "Update login.php"
    This reverts commit 7cb368e87f7b11611d6c5f27f25d5cba0b15899d.

--- login.php
+++ login.php
@@ -51,7 +51,9 @@
 																		</p>
 																	</form>
 															</div>
-														
+															<div class="card-footer text-center py-3">
+																	<div class="small"><a href="register.html">Need an account? Sign up!</a></div>
+															</div>
 													</div>
 											</div>
 									</div>
