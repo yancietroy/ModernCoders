479c9a3a8f384de696aeca2565b87ea1b0c2588b
stycroft <98692180+stycroft@users.noreply.github.com>
2022-06-25 14:20:50 +0800

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>JRU Student Organizations Portal Login Page</title>
 	<link rel="stylesheet" type="text/css" title="stylesheet" href="assets/css/style.css">
 
@@ -11,7 +11,56 @@
 </head>
 
 <body class="bg">
-	<section class="vh-100">
+	<div id="layoutAuthentication">
+			<div id="layoutAuthentication_content">
+					<main>
+							<div class="container">
+									<div class="row justify-content-center">
+											<div class="col-lg-5">
+													<div class="card shadow-lg border-0 rounded-lg mt-5">
+															<div class="card-body text-center">
+																<div class="row g-0 justify-content-center mt-2">
+																		<div class="col-xs-12 col-md-3 col-md-offset-3 mb-4">
+																				<img class="mb-3 mx-auto d-none d-md-block" src="assets/img/csc-logo.png" alt="" width="82" height="80">
+																					</div>
+																				<div class="col-xs-12 col-md-4 col-md-offset-3 mb-4">
+																				<img class="mb-3 mx-auto d-none d-md-block" src="assets/img/jru-logo.png" alt="" width="110" height="110">
+																						</div>
+																						<div class="col-xs-12 col-md-3 col-md-offset-3 mb-4">
+																				<img class="mb-3 mx-auto d-none d-md-block" src="assets/img/comsoc-logo.png" alt="" width="82" height="80">
+																					</div>
+														</div>
+																<p class="h5 mb-2">JRU Student Organizations Portal</p>
+																	<form>
+																		<h1 class="fs-4 card-title fw-bold mb-3 text-uppercase">Login</h1>
+
+																		<div class="form-floating mb-3">
+																			<input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" required>
+																			<label class="text-muted" for="email">Email address</label>
+																		</div>
+
+																		<div class="form-floating mb-2">
+																			<input type="password" class="form-control" id="password" name="password" value="" placeholder="password" required>
+																			<label class=" text-muted " for="password">Password</label>
+																		</div>
+
+																		<button class="w-100 btn btn-lg btn-primary mt-4" type="submit" name='submit'>Sign in</button>
+
+																		<hr class="my-4">
+																		<p class="mt-3">Don't have an account? <a href="register.php" class="text-blue-50 fw-bold">Register</a>
+																		</p>
+																	</form>
+															</div>
+															<div class="card-footer text-center py-3">
+																	<div class="small"><a href="register.html">Need an account? Sign up!</a></div>
+															</div>
+													</div>
+											</div>
+									</div>
+							</div>
+					</main>
+			</div>
+	<!--<section class="vh-100">
 		<div class="container py-5 h-100">
 			<div class="row d-flex justify-content-center align-items-center h-100">
 				<div class="col-12 col-md-8 col-lg-6 col-xl-5">
@@ -29,9 +78,8 @@
 											<img class="mb-3 mx-auto d-none d-md-block" src="assets/img/comsoc-logo.png" alt="" width="82" height="80">
 												</div>
 					</div>
-							<p class="h5 mb-2">JRU Student Organizations Portal</p>
-
-							  <form method="POST" class="needs-validation" novalidate="" autocomplete="off">
+							<p class="h5 mb-2">JRU Student Organizations Portal</p>-->
+							  <!--form method="POST" class="needs-validation" novalidate="" autocomplete="off">
 								<form>
 									<h1 class="fs-4 card-title fw-bold mb-3 text-uppercase">Login</h1>
 
@@ -57,8 +105,8 @@
 				</div>
 			</div>
 
-		</div>
-		<!--
+		</div>!-->
+
 		<div id="layoutAuthentication_footer">
 				<footer class="py-2 bg-light mt-5">
 						<div class="container-fluid px-4">
@@ -68,7 +116,6 @@
 						</div>
 				</footer>
 		</div>
-	!-->
 		<script src="js/login.js"></script>
 		<?php
 if(isset ($_POST['submit']))
