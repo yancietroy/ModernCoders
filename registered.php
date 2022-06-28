06387e4698d926e802da763519ab82c02b588981
bienny <106976531+bienny@users.noreply.github.com>
2022-06-28 23:04:12 +0800

    Update registered.php

--- registered.php
+++ registered.php
@@ -31,7 +31,7 @@
 		else if (isset ($_POST['submit']))
 		{
 			include('mysql_connect.php');
-			$query = "INSERT INTO tb_students(STUDENT_ID, FIRST_NAME, LAST_NAME, MIDDLE_NAME, AGE, GENDER, YEAR_LEVEL,  COURSE, EMAIL, PASSWORD) VALUES('$si', '$fn', '$ln', '$mn', '$age', '$g', '$yl', '$course', '$e', SHA('$pass'))";
+			$query = "INSERT INTO tb_students(STUDENT_ID, FIRST_NAME, LAST_NAME, MIDDLE_NAME, BIRTHDATE, AGE, GENDER, YEAR_LEVEL,  COURSE, EMAIL, PASSWORD) VALUES('$si', '$fn', '$ln', '$mn', '$date', '$age', '$g', '$yl', '$course', '$e', SHA('$pass'))";
 			$result = @mysqli_query($conn, $query);
 
 			echo "  <h3>You are now registered</h3>";
