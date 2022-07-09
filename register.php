900aad8ae28ce0358b6a63c4f18eab693a9d6e82
bienny <106976531+bienny@users.noreply.github.com>
2022-07-09 18:14:06 +0800

    Update register.php

--- register.php
+++ register.php
@@ -136,9 +136,15 @@
                     <label class="form-label select-label" id="asterisk">College</label>
                     <select class="form-select form-select-sm" name="college" id="select-group" required>
                       <option class="greyclr" selected disabled value="" text-muted>Select College</option>
-                      <option value="1">One</option>
-                      <option value="2">Two</option>
-                      <option value="3">Three</option>
+                      <?php 
+                            include('mysql_connect.php');
+                            $query = "SELECT college FROM tb_collegedept";
+                            $result = @mysqli_query($conn, $query);
+                            while($data = @mysqli_fetch_array($result)) {           
+                                echo '<option value="'.$data[0].'">'.$data[0].'</option>';  
+                            }
+                            @mysqli_close($conn);
+                      ?>
                     </select>
                     <div class="invalid-feedback">
                       Please select a college program
