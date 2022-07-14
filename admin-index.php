e817432dc3db3a30d2348112b0a60cd6f9a4536b
bienny <106976531+bienny@users.noreply.github.com>
2022-07-14 11:15:39 +0800

    Update admin-index.php

--- admin-index.php
+++ admin-index.php
@@ -1,6 +1,7 @@
 <?php
 ob_start();
 session_start();
+$id = $_SESSION['use'];
 if(isset($_SESSION['msg'])){
     print_r($_SESSION['msg']);#display message
     unset($_SESSION['msg']); #remove it from session array, so it doesn't get displayed twice
