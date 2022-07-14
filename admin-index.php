fb6bbb00e5d96a0fdb2f32f8118942e04ec09f40
stycroft <98692180+stycroft@users.noreply.github.com>
2022-07-14 11:50:02 +0800

    Update admin-index.php

--- admin-index.php
+++ admin-index.php
@@ -128,7 +128,11 @@ if(isset($_SESSION['msg'])){
               <li class="nav-item dropdown">
                 <a class="nav-link" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" data-display="static" aria-haspopup="true" aria-expanded="false">
                   <img class="rounded-circle me-lg-2" src="assets/img/img_avatar.png" alt="" style="width: 40px; height: 40px;border: 2px solid #F2AC1B;">
-                  <span class="d-none d-lg-inline-flex">John Doe</span></a>
+                  <span class="d-none d-lg-inline-flex"><?php $query = "SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS name FROM tb_admin WHERE ADMIN_ID = '$id'";
+                      $result = @mysqli_query($conn, $query);
+                      $row = mysqli_fetch_array ($result);
+                      if ($row)
+                      { echo "$row[0]"; } ?></span></a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                   <li><a class="dropdown-item" href="#!">Profile</a></li>
                   <li><a class="dropdown-item" href="#!">Settings</a></li>
