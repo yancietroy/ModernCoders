4354b7762820605d7ebb59f1868f7c51865e3725
bienny <106976531+bienny@users.noreply.github.com>
2022-07-19 15:25:23 +0800

    Changes

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -1,62 +1,20 @@
-$(document).ready(function() {
-  $("form[name='form']").validate({
-          rules: {
-                first_name: "required",
-                last_name: "required",
-                birthdate: "required",
-                age: "required",
-                gender: "required",
-                studentid: "required",
-                school_year: "required",
-                section: "required",
-                college: "required",
-                course: "required",
-                org: "required",
-                email: "required",
-                password: "required",
-                confirmpassword: "required": {
-                  required: true,
-              },
-              /*
-              txtConfirmPassword: {
-                                required: true,
-                                minlength: 8,
-                                equalTo: "#txtNewPassword"
-                            },
-                            email: {
-                                required: true,
-                                email: true
-                            }
-                          }
-*/
+(function() {
+  'use strict'
+  const forms = document.querySelectorAll('.requires-validation')
+  Array.from(forms)
+    .forEach(function(form) {
+      form.addEventListener('submit', function(event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
 
-          messages: {
-             first_name: "Please provide a valid name.",
-              name: "Please provide a valid name.",
-               name: "Please provide a valid name.",
-                name: "Please provide a valid name.",
-                 name: "Please provide a valid name.",
-                  name: "Please provide a valid name.",
-                   name: "Please provide a valid name.",
-                    name: "Please provide a valid name.",
-                     name: "Please provide a valid name.",
-                      name: "Please provide a valid name.",
-                       name: "Please provide a valid name.",
-                        name: "Please provide a valid name.",
-                         name: "Please provide a valid name.",
-                          name: "Please provide a valid name.",
-                           name: "Please provide a valid name.",
-              password: {
-                  required: "Please provide a password",
-                  minlength: "Your password must be at least 8 characters long"
-              },
-              confirmpassword: {
-                  required: "Please provide a password",
-                  minlength: "Your password must be at least 8 characters long",
-                  equalTo: "Please match the password entered"
-              },
-              submitHandler: function (form) {
-                 form.submit();
-          }
-      });
-          });
+        form.classList.add('was-validated')
+      }, false)
+    })
+})();
+$(document).ready(function() {
+  $('#sidebarCollapse').on('click', function() {
+    $('#sidebar').toggleClass('active');
+  });
+});
---
+++ assets/js/login.js
@@ -0,0 +1,27 @@
+function createRipple(event) {
+
+	const button = event.currentTarget;
+	const btnRect = button.getBoundingClientRect();
+	const circle = document.createElement("span");
+	const diameter = Math.max(btnRect.width, btnRect.height);
+	const radius = diameter / 2;
+
+	circle.style.width = circle.style.height = `${diameter}px`;
+	circle.style.left = `${event.clientX - (btnRect.left + radius)}px`;
+	circle.style.top = `${event.clientY - (btnRect.top + radius)}px`;
+	circle.classList.add("ripple");
+
+	const ripple = button.getElementsByClassName("ripple")[0];
+
+	if (ripple) {
+		ripple.remove();
+	}
+
+	button.appendChild(circle);
+}
+
+const buttons = document.getElementsByClassName("ct-link-button");
+
+for (const button of buttons) {
+	button.addEventListener("click", createRipple);
+}
