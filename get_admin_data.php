4356d126d061f09d2ae884fc1548f0307132e2e2
bienny <106976531+bienny@users.noreply.github.com>
2022-08-04 16:33:46 +0800

    Deleted tests

--- get_admin_data.php
+++
@@ -1,7 +0,0 @@
-<?php include('mysql_connect.php');
-$si = $_POST['STUDENT_ID'];
-$sql = "SELECT * FROM tb_students WHERE STUDENT_ID='$si' LIMIT 1";
-$query = mysqli_query($conn,$sql);
-$row = mysqli_fetch_assoc($query);
-echo json_encode($row);
-?>  
