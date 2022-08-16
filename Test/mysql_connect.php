060f6d38f95a32545e153413818baaf384c5bbf3
bienny <106976531+bienny@users.noreply.github.com>
2022-08-17 00:02:53 +0800

    Test

---
+++ Test/mysql_connect.php
@@ -0,0 +1,14 @@
+<?php
+/*
+------------------------------------------------------------------------------------------------------
+Script Name: mysql_connect.php
+Author: <type your name>
+Description: To connect to the MySQL server and database
+------------------------------------------------------------------------------------------------------
+*/
+$username ="root";
+$password="";
+$database="dbstudentorgportal";
+$conn = mysqli_connect("localhost",$username,$password);
+mysqli_select_db($conn, $database) or die ("Unable to select database");
+?>
---
+++ Test/signatory-update-button.php
@@ -0,0 +1,25 @@
+<?php
+    include('mysql_connect.php');
+
+    if (isset($_POST['updatedata']) || isset($pr) || isset($s))
+    {
+        $id = $_POST['project_id'];
+        $pr = $_POST['project_remarks'];
+        $s = $_POST['status'];
+
+        $query = "SELECT * FROM `tb_projectmonitoring`;";
+        $result = @mysqli_query($conn, $query);
+        $row = mysqli_fetch_array($result);
+
+        if($row)
+        {
+        $query = "UPDATE `tb_projectmonitoring` SET `status` = '$s', `remarks` ='$pr' WHERE `project_id` = '$id';";
+        $result = @mysqli_query($conn, $query);
+        echo "<script type='text/javascript'>
+              alert('Status updated!')
+              </script>";
+        header("Location:signatory-masterlist.php");
+        }
+    }
+@mysqli_close($conn);
+?>
