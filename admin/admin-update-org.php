75b548f9377884d87abcdd8a18b30d9470b654ae
bienny <106976531+bienny@users.noreply.github.com>
2022-10-13 18:36:47 +0800

    Modal org fixes

---
+++ admin/admin-update-org.php
@@ -0,0 +1,22 @@
+<?php
+include('../mysql_connect.php');
+ob_start();
+session_start();
+if (isset ($_POST['updatedata']))
+{
+
+    $orgid = $_POST["ORG_ID"];
+    $orgName = = $_POST["ORG"];
+    $query = "SELECT * FROM tb_orgs";
+    $result = @mysqli_query($conn, $query);
+    $row = @mysqli_fetch_array($result);
+
+		if($row)
+		{
+			$query = "UPDATE `tb_orgs` SET `ORG` = '$orgName' WHERE `ORG_ID` = '$orgid'";
+			$result = @mysqli_query($conn, $query);
+			$_SESSION['message'] = '<script>alert("Update Successful")</script>';
+			header("Location:admin-orgs.php");
+		}
+}
+?>
